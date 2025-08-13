45ea3ccd8d93d067dbf5337968474a2c5fbf6d50
Tristan Yi <133058934+tristanyii@users.noreply.github.com>
2025-08-12 21:16:28 -0400

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,14 @@
+# Last updated: 8/12/2025, 9:16:28 PM
+class Solution(object):
+    def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
