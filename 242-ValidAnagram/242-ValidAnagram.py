b57d08b215f4dfa3a624fc90b7f441b7853b3c24
Tristan Yi <133058934+tristanyii@users.noreply.github.com>
2025-08-16 23:46:25 -0400

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,12 @@
+# Last updated: 8/16/2025, 11:46:24 PM
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
