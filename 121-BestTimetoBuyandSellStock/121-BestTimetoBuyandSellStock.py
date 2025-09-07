fe3057afeb95b9541791e7ccb2559aa2d006c40d
Tristan Yi <133058934+tristanyii@users.noreply.github.com>
2025-09-07 17:37:12 -0400

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -0,0 +1,19 @@
+# Last updated: 9/7/2025, 5:37:11 PM
+class Solution(object):
+    def maxProfit(self, prices):
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
+
+        
+
+
+        
