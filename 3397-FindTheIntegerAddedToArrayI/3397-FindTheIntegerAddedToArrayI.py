b2e561940015cf4ec75b876df0cf5d4fec297c71
Tristan Yi <133058934+tristanyii@users.noreply.github.com>
2026-01-12 15:13:29 -0500

    Create 3397-FindTheIntegerAddedToArrayI

---
+++ 3397-FindTheIntegerAddedToArrayI/3397-FindTheIntegerAddedToArrayI.py
@@ -0,0 +1,6 @@
+# Last updated: 1/12/2026, 3:13:29 PM
+class Solution:
+    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+       n = max(nums1)
+       m = max(nums2)
+       return m-n
