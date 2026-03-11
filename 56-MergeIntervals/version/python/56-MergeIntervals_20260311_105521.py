7d1c1502b01be1ae4684fd25ba61341f963c4957
Tristan Yi <133058934+tristanyii@users.noreply.github.com>
2026-03-11 10:55:22 -0400

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/version/python/56-MergeIntervals_20260311_105521.py
@@ -0,0 +1,32 @@
+# Last updated: 3/11/2026, 10:55:21 AM
+1class Solution:
+2    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+3        '''
+4        1 <---> 3
+5            2-------->6
+6        1 <---------->6
+7        '''
+8
+9        if not intervals:
+10            return []
+11        intervals.sort()
+12        first_interval = intervals[0]
+13        ans = []
+14
+15        for interval in intervals[1::]:
+16            merge = 0
+17            if first_interval[1] >= interval[0]:
+18                first_interval[1] = max(first_interval[1], interval[1])
+19            else:
+20                ans.append(first_interval)
+21                first_interval = interval
+22        ans.append(first_interval)
+23        return ans
+24            
+25
+26
+27
+28        
+29
+30
+31
