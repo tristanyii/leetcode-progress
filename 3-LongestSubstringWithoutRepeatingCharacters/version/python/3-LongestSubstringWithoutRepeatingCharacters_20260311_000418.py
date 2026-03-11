830dd66a5a6223d732d0eeb8fc4d09458b76e5a8
Tristan Yi <133058934+tristanyii@users.noreply.github.com>
2026-03-11 00:04:19 -0400

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/version/python/3-LongestSubstringWithoutRepeatingCharacters_20260311_000418.py
@@ -0,0 +1,16 @@
+# Last updated: 3/11/2026, 12:04:18 AM
+1class Solution:
+2    def lengthOfLongestSubstring(self, s: str) -> int:
+3        l = 0
+4        checkSet = set()
+5        longest = 0
+6        for r in range(len(s)):
+7            while s[r] in checkSet:
+8                checkSet.remove(s[l])
+9                l += 1
+10            else:
+11                checkSet.add(s[r])
+12                longest = max(longest, r - l + 1)
+13        return longest
+14
+15
