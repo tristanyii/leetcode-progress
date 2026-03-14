4a8b53dee62b0e03528e93f7243fac3ac229b8ec
Tristan Yi <133058934+tristanyii@users.noreply.github.com>
2026-03-14 01:27:26 -0400

    Create 560-SubarraySumEqualsK

---
+++ 560-SubarraySumEqualsK/version/python/560-SubarraySumEqualsK_20260314_012726.py
@@ -0,0 +1,13 @@
+# Last updated: 3/14/2026, 1:27:26 AM
+1class Solution:
+2    def subarraySum(self, nums, k):
+3        prefix_sums = defaultdict(int)
+4        counts = 0
+5        prefix_sum = 0
+6        prefix_sums[0] = 1
+7
+8        for num in nums:
+9            prefix_sum += num
+10            counts += prefix_sums[prefix_sum - k]
+11            prefix_sums[prefix_sum] += 1
+12        return counts
