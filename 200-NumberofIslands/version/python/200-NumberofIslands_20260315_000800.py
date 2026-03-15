cba05cda60e47e2058ac222cbe039b3db155f3c9
Tristan Yi <133058934+tristanyii@users.noreply.github.com>
2026-03-15 00:08:00 -0400

    Create 200-NumberofIslands

---
+++ 200-NumberofIslands/version/python/200-NumberofIslands_20260315_000800.py
@@ -0,0 +1,36 @@
+# Last updated: 3/15/2026, 12:08:00 AM
+1class Solution:
+2    def numIslands(self, grid: List[List[str]]) -> int:
+3        if not grid:
+4            return 0 
+5        
+6        rows = len(grid)
+7        columns = len(grid[0])
+8        islands = 0
+9
+10        def dfs(grid, r, c):
+11            if not grid:
+12                return
+13            if r < 0 or c < 0 or r >= rows or c >= columns:
+14                return
+15            if grid[r][c] == '0':
+16                return
+17
+18            grid[r][c] = '0'
+19            dfs(grid, r+1, c)
+20            dfs(grid, r-1, c)
+21            dfs(grid, r, c+1)
+22            dfs(grid, r, c-1)
+23
+24
+25        for r in range(rows):
+26            for c in range(columns):
+27                if grid[r][c] == '1':
+28                    islands += 1
+29                    dfs(grid, r, c)
+30
+31        return islands
+32        
+33
+34        
+35        
